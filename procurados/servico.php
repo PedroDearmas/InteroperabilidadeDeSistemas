9a3fa4dffdc4e726ef60fd39a57ab131c958e948
Pedro Araujo Dearmas <115725683+PedroDearmas@users.noreply.github.com>
2024-02-22 22:08:04 -0300

    Aula 22/02

--- procurados/servico.php
+++ procurados/servico.php
@@ -1,9 +1,9 @@
 <?php
 
     $txt = file_get_contents('php://input');
-    json_decode($txt, true);
+    $obj = json_decode($txt, true);
     $cpf = $obj ['cpf'];
-    $consulta = "select * from pessoa cpf = '$cpf';";
+    $consulta = "select * from pessoa where cpf = '$cpf';";
     $conexao = new pdo ('sqlite:bd');
     $resultado = $conexao->query($consulta)->fetchAll();
     if(count ($resultado) > 0){
